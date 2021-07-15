8ed16888664f70cafd1e91c9821108418313e9f7
Cristina <crigraiba@gmail.com>
2021-07-15 20:33:57 +0200

    postman

--- src/main/java/com/example/controller/UserRestController.java
+++ src/main/java/com/example/controller/UserRestController.java
@@ -8,9 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.example.service.UserService;
 import com.example.domain.User;
@@ -29,7 +29,6 @@ public class UserRestController {
 	
 	@GetMapping("/{id}")
 	public User readById(@PathVariable String id) {
-		System.out.println(id);
 		return service.readById(id);
 	}
 	
