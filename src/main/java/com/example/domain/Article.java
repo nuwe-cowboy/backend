244e8760326f7e80a70578b70f7f160b010bbd70
Cristina <crigraiba@gmail.com>
2021-07-27 23:16:28 +0200

    article input validation

--- src/main/java/com/example/domain/Article.java
+++ src/main/java/com/example/domain/Article.java
@@ -2,9 +2,15 @@ package com.example.domain;
 
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.MongoId;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import org.springframework.data.mongodb.core.mapping.Field;
 
 import java.time.LocalDateTime;
+
+import javax.validation.constraints.NotBlank;
 
 import org.bson.types.ObjectId;
 
@@ -12,13 +18,17 @@ import org.bson.types.ObjectId;
 public class Article {
 	
 	@MongoId
-	@Field(name = "_id")
+	@Field("_id")
+	@JsonProperty("id")
 	private ObjectId id;
-	@Field(name = "title")
+	@Field("title")
+	@NotBlank(message = "Title is mandatory")
 	private String title;
-	@Field(name = "body")
+	@Field("body")
+	@NotBlank(message = "Body is mandatory")
 	private String body;
-	@Field(name = "timestamp")
+	@Field("timestamp")
+	@JsonFormat(pattern = "dd-MM-yyyy hh:mm:ss")
 	private LocalDateTime timestamp;
 	
 	public Article() {
