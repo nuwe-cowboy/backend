c4e4bba7fb256bff192f7ad41f3f8e2604289eb9
Cristina <crigraiba@gmail.com>
2021-08-10 16:41:42 +0200

    event postman and organize imports

--- src/main/java/com/example/controller/ArticleRestController.java
+++ src/main/java/com/example/controller/ArticleRestController.java
@@ -6,19 +6,19 @@ import java.util.UUID;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RestController;
 
-import com.example.service.ArticleService;
 import com.example.domain.Article;
+import com.example.service.ArticleService;
 
 @RestController
 @RequestMapping("/articles")
--- src/main/java/com/example/repository/IArticleRepository.java
+++ src/main/java/com/example/repository/IArticleRepository.java
@@ -1,9 +1,9 @@
 package com.example.repository;
 
-import org.springframework.stereotype.Repository;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
 import java.util.UUID;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.domain.Article;
 
